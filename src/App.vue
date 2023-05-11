32904b80b460735deb38e9f19a26ccdd0a724e2b
Dima <skaizy44@gmail.com>
2023-05-11 15:26:16 +0300

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 <template>
   <h1>hello</h1>
-  <my-component />
+  <!-- <my-component /> -->
+  <h2>Вивід мешканців квартири:</h2>
+  <my-notes
+    v-for="note in notes"
+    v-bind:key="note.id"
+    v-bind:myName="note.name"
+    v-bind:myAge="note.age"
+  />
 </template>
 
 <script>
+import MyNotes from "./assets/MyNotes.vue";
+
 export default {
-  name: "App",
+  data() {
+    return {
+      notes: [
+        {
+          name: "Dima",
+          age: 35,
+          id: 1,
+        },
+        {
+          name: "Lesia",
+          age: 28,
+          id: 2,
+        },
+        {
+          name: "Kuma",
+          age: 3,
+          id: 3,
+        },
+        {
+          name: "Simka",
+          age: 1,
+          id: 3,
+        },
+      ],
+    };
+  },
+
+  components: {
+    MyNotes,
+  },
 };
 </script>
 
---
+++ src/assets/MyNotes.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>імья - {{ myName }}</p>
+  <p>age - {{ myAge }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    myName: {
+      type: String,
+      required: true,
+      default: "not have",
+      validator(value) {
+        if (typeof value === "string") {
+          return true;
+        }
+      },
+    },
+    myAge: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
